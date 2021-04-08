e10cc55384f1d38e529a81f5795d4b485b9ba71b
Anton D <anton.drugge@gmail.com>
2021-04-08 15:15:40 +0200

    lagt till filer

---
+++ exjobb-vue/src/components/AddBooking.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="addBooking">
+    <button @click="increment">Count is: {{state.count}}, double is: {{state.double}}</button>
+  </div>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+//import states from "../assets/bookinglist.json";
+
+export default {
+  name: "AddBooking",
+
+  setup() {
+
+    return {
+      state,
+      increment
+    };
+  }
+};
+</script>
