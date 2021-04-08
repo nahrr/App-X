06aa55e2144f9cfa2f1779ada5432bb31eb3f5c6
Johan Kjellström <48734017+nahrr@users.noreply.github.com>
2021-04-08 16:54:51 +0200

    Refactor

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- <Count /> -->
+    <AddBooking />
+  </div>
+</template>
+
+<script>
+//import Count from "@/components/Count";
+import AddBooking from "./components/AddBooking.vue";
+export default {
+  name: "app",
+  components: { AddBooking },
+};
+</script>
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AddBooking.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>Bokningar</h1>
+  <div class="add_Booking">
+    <button v-on:click="getBookingsData">Get Bookings</button>
+    <div v-for="bookingData in bookingList" :key="bookingData.id" class="booking-data">
+      <div class="bookings">
+        <span>{{ bookingData.location }} </span>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddBooking",
+  data() {
+    return {
+      bookingList: [],
+    };
+  },
+  methods: {
+    getBookingsData() {
+      fetch("bookinglist.json")
+        .then((response) => response.json())
+        .then((data) => (this.bookingList = data));
+    },
+  },
+};
+</script>
+
+<style scoped>
+.booking-data{
+    display: flex;
+  align-items: center;
+  margin-top: 20px;
+  margin-left: 20px;
+  border-bottom: 2px solid #ccc;
+  padding: 20px;
+}
+
+.bookings {
+  flex-grow: 8;
+  text-align: left;
+  padding-left: 20px;
+}
+button {
+  padding: 10px;
+  background-color: #1aa832;
+  color: white;
+  border: 1px solid #ccc;
+}
+</style>
