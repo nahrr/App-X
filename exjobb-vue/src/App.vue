309af548af1892f333785f14f77d26aeb81a7ac0
Johan Kjellström <48734017+nahrr@users.noreply.github.com>
2021-04-08 14:44:35 +0200

    Composition api

--- exjobb-vue/src/App.vue
+++ exjobb-vue/src/App.vue
@@ -1,37 +1,13 @@
 <template>
   <div id="app">
-    <nav>
-      <div class="navigation_user">
-        {{ user.username }}
-      </div>
-    </nav>
-    <Test/>
+    <Count />
   </div>
 </template>
 
 <script>
-import Test from "./components/Test.vue";
-
+import Count from "@/components/Count";
 export default {
-  name: "App",
-  components: { Test },
-  data(){
-    return {
-      user: {
-        
-      }
-    }
-  }
+  name: "app",
+  components: { Count }
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
