456e6bd8b72514f0d56ccfaa1d6382a99e6dfa41
Johan Kjellström <48734017+nahrr@users.noreply.github.com>
2021-04-08 18:27:40 +0200

    Added new import of jsonfile

--- src/components/AddBooking.vue
+++ src/components/AddBooking.vue
@@ -1,52 +1,64 @@
 <template>
   <div>
     <h1>Bokningar</h1>
-  <div class="add_Booking">
-    <button v-on:click="getBookingsData">Get Bookings</button>
-    <div v-for="bookingData in bookingList" :key="bookingData.id" class="booking-data">
-      <div class="bookings">
-        <span>{{ bookingData.location }} </span>
+    <div class="add_Booking">
+      <button v-on:click="showBookings">Get Bookings</button>
+      <div
+        v-for="bookingData in bookingList"
+        :key="bookingData.id"
+        class="booking-data"
+      >
+        <div class="bookings">
+          <div>
+            {{ bookingData.country }}
+          </div>
+          <div>
+            {{ bookingData.location }} 
+          </div>
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
+import states from "../assets/bookinglist.json";
 export default {
   name: "AddBooking",
-  data() {
-    return {
-      bookingList: [],
-    };
-  },
-  methods: {
-    getBookingsData() {
-      fetch("bookinglist.json")
-        .then((response) => response.json())
-        .then((data) => (this.bookingList = data));
+  computed: {
+    bookingList() {
+      return states;
     },
   },
+  methods:{
+    showBookings(){
+ 
+    }
+  }
 };
+
 </script>
 
 <style scoped>
-.booking-data{
-    display: flex;
+.booking-data {
+  display: flex;
   align-items: center;
-  margin-top: 20px;
-  margin-left: 20px;
+  margin-top: 1rem;
+  margin: 0 auto;
   border-bottom: 2px solid #ccc;
-  padding: 20px;
+  padding: 1rem;
+  max-width: 55rem;
 }
 
 .bookings {
   flex-grow: 8;
   text-align: left;
-  padding-left: 20px;
+  padding-left: 1rem;
+  display: flex;
+  justify-content: space-between;
 }
 button {
-  padding: 10px;
+  padding: 1rem;
   background-color: #1aa832;
   color: white;
   border: 1px solid #ccc;
