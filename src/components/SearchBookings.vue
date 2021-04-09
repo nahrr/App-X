d5ac3651bad0b27fb05e8fd5a3b0526b624f3670
Johan Kjellström <48734017+nahrr@users.noreply.github.com>
2021-04-09 15:39:56 +0200

    refactor

--- src/components/SearchBookings.vue
+++ src/components/SearchBookings.vue
@@ -1,17 +1,28 @@
 <template>
-  <div id="app">
-    <div class="search-wrapper">
-    <label>Search title:</label>
+  <div>
+    <div class="search_panel">
+    <label>Search booking:</label>
     <input type="text" v-model="search" placeholder="Search Bookings.." />
       
     </div>
     <div class="wrapper">
-      <!-- <div class="card" v-for="booking in filteredList"> -->
+      <!-- <div class="card" 
+      v-for="filterBooking in filteredList"
+      :key="filterBooking.id"
+       >
+        <div class="bookings">
+            <div>
+              {{ bookingData.country }}
+            </div>
+       
+     </div> -->
+    <!-- </div> -->
     </div>
   </div>
 </template>
 
 <script>
+//import states from "../assets/bookinglist.json";
 export default {
   name: "SearchBookings",
   props: {
@@ -20,17 +31,30 @@ export default {
       required: true,
     },
   },
+  methods: {
+    bar: function () {
+      this.$root.$refs.ShowBookings.showBookings();
+    },
+  },
 
-  //   computed: {
-  //     filteredList() {
-  //       return this.postList.filter(post => {
-  //         return post.location.toLowerCase().includes(this.search.toLowerCase())
-  //       })
-  //     }
-  //   }
+  computed: {
+    filteredList() {
+      return this.bar.filter((post) => {
+        return post.location.toLowerCase().includes(this.search.toLowerCase());
+      });
+    },
+  },
 };
 </script>
 
 <style scoped>
-
+.search_panel {
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+  margin: 0 auto;
+  border-bottom: 2px solid #ccc;
+  padding: 1rem;
+  max-width: 55rem;
+}
 </style>
